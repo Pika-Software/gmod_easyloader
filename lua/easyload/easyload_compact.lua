8324168b793c9a271bfe08459b8a2a8a91946a00
PrikolMen:-b <44779902+PrikolMen@users.noreply.github.com>
2021-10-03 13:49:33 +0400

    Update easyload_compact.lua

--- lua/easyload/easyload_compact.lua
+++ lua/easyload/easyload_compact.lua
@@ -1,61 +1,59 @@
+--  EasyLoader Compact by PrikolMen#3372
 if EasyLoader then return end
 EasyLoader = {}
 
-// Gmod Libs Preload
+// Preload
+local string_EndsWith = string.EndsWith
 local AddCSLuaFile = AddCSLuaFile
-local MsgC = MsgC
 local string_lower = string.lower
 local string_Left = string.Left
-local include = include
 local file_Find = file.Find
+local include = include
 local ipairs = ipairs
-local string_EndsWith = string.EndsWith
+local Color = Color
+local MsgC = MsgC
 //
 
-/*
-    Easy Lua Loader Compact by PrikolMen#3372
-*/
-
-local function Log(tag, fl)
+function EasyLoader:Log(tag, fl)
     MsgC(Color(40, 192, 252), "["..(tag or "ESL").."] ", Color(255, 194, 61), tag and (fl..": OK") or ("SV INCLUDE: "..fl), "\n")
 end
 
-function EasyLoader.Include(fl, dir, tag)
+function EasyLoader:Include(fl, dir, tag)
     local fileSide = string_lower(string_Left(fl , 3))
 
     if SERVER and fileSide == "sv_" then
         include(dir..fl)
-        Log(tag, fl)
+        self:Log(tag, fl)
     elseif fileSide == "sh_" then
         if SERVER then 
             AddCSLuaFile(dir..fl)
-            Log(tag, fl)
+            self:Log(tag, fl)
         end
         include(dir..fl)
-        Log(tag, fl)
+        self:Log(tag, fl)
     elseif fileSide == "cl_" then
         if SERVER then 
             AddCSLuaFile(dir..fl)
-            Log(tag, fl)
+            self:Log(tag, fl)
         elseif CLIENT then
             include(dir..fl)
-            Log(tag, fl)
+            self:Log(tag, fl)
         end
     end
 end
 
-function EasyLoader.Load(dir, tag)
+function EasyLoader:Load(dir, tag)
     dir = dir .. "/"
     local fl, folder = file_Find(dir.."*", "LUA")
 
     for k, v in ipairs(fl) do
         if string_EndsWith(v, ".lua") then
-            EasyLoader.Include(v, dir, tag)
+            self:Include(v, dir, tag)
         end
     end
     
     for k, v in ipairs(folder) do
-        EasyLoader.Load(dir..v, tag)
-        Log(tag, v)
+        self:Load(dir..v, tag)
+        self:Log(tag, v)
     end
 end
