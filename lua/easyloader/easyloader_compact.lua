90c97fb197582aebe7c19b75a43e9ca2ef752452
PrikolMen:-b <44779902+PrikolMen@users.noreply.github.com>
2021-12-02 14:55:36 +0400

    Introducing `Easy Loader` v2.5

--- lua/easyloader/easyloader_compact.lua
+++ lua/easyloader/easyloader_compact.lua
@@ -1,59 +1,129 @@
---  EasyLoader Compact by PrikolMen#3372
-if EasyLoader then return end
-EasyLoader = {}
+-- Easy Loader by PrikolMen#3372
+local version = 2.5
+if EasyLoader and (EasyLoader["_VERSION"] > version) then return end
+EasyLoader = {
+    ["_VERSION"] = version,
+    ["_COLOR"] = {
+        ["Text"] = Color(250, 238, 255),
+        ["Client"] = Color(255, 193, 7),
+        ["Server"] = Color(40, 192, 252)
+    }
+}
 
-// Preload
 local string_EndsWith = string.EndsWith
 local AddCSLuaFile = AddCSLuaFile
 local string_lower = string.lower
 local string_Left = string.Left
 local file_Find = file.Find
+local table_Add = table.Add
 local include = include
 local ipairs = ipairs
-local Color = Color
+local unpack = unpack
 local MsgC = MsgC
-//
 
-function EasyLoader:Log(tag, fl)
-    MsgC(Color(40, 192, 252), "["..(tag or "ESL").."] ", Color(255, 194, 61), tag and (fl..": OK") or ("SV INCLUDE: "..fl), "\n")
+function EasyLoader:SideColor()
+	return CLIENT and self["_COLOR"]["Client"] or self["_COLOR"]["Server"]
 end
 
-function EasyLoader:Include(fl, dir, tag)
+function EasyLoader:Log(tag, ...)
+    MsgC(self:SideColor(), "["..(tag or "EasyLoader").."] ", self["_COLOR"]["Text"], unpack(table_Add({...}, {"\n"})))
+end
+
+function EasyLoader:BuildPath(dir, fl)
+	if isstring(dir) then
+		if isstring(fl) then
+			if not string_EndsWith(dir, "/") then
+				dir = dir .. "/"
+			end
+		else
+			return false
+		end
+	elseif not isstring(fl) then
+		return false
+	end
+
+	return (dir or "") .. (fl or "")
+end
+
+function EasyLoader:Include(dir, fl, tag)
     local fileSide = string_lower(string_Left(fl , 3))
+    local path = self:BuildPath(dir, fl)
 
-    if SERVER and fileSide == "sv_" then
-        include(dir..fl)
+    if SERVER and (fileSide == "sv_") then
+        include(dir .. fl)
         self:Log(tag, fl)
-    elseif fileSide == "sh_" then
-        if SERVER then 
-            AddCSLuaFile(dir..fl)
-            self:Log(tag, fl)
+    elseif (fileSide == "sh_") then
+        if SERVER then
+            AddCSLuaFile(dir .. fl)
         end
-        include(dir..fl)
+
+        include(dir .. fl)
         self:Log(tag, fl)
-    elseif fileSide == "cl_" then
-        if SERVER then 
-            AddCSLuaFile(dir..fl)
-            self:Log(tag, fl)
+    elseif (fileSide == "cl_") then
+        if SERVER then
+            AddCSLuaFile(dir .. fl)
         elseif CLIENT then
-            include(dir..fl)
+            include(dir .. fl)
             self:Log(tag, fl)
         end
     end
 end
 
-function EasyLoader:Load(dir, tag)
-    dir = dir .. "/"
-    local fl, folder = file_Find(dir.."*", "LUA")
+local client = {"_cl", "cl", "client", "cln", "cli", "clnt"}
+local server = {"_sv", "sv", "server", "srv", "serv"}
 
-    for k, v in ipairs(fl) do
-        if string_EndsWith(v, ".lua") then
-            self:Include(v, dir, tag)
+function EasyLoader:IsRightSide(path)
+    for _, str in ipairs(string.Split(path, "/")) do
+        for _, srv in ipairs(server) do
+            if (str:lower() == srv) then
+                return SERVER
+            end
+        end
+
+        for _, cl in ipairs(client) do
+            if (str:lower() == cl) then
+                return CLIENT
+            end
         end
     end
-    
-    for k, v in ipairs(folder) do
-        self:Load(dir..v, tag)
-        self:Log(tag, v)
+
+    return true
+end
+
+function EasyLoader:Load(dir, tag, depth)
+	assert(type(dir) == "string", "bad argument #1 (string expected)")
+
+    if (dir != "") then
+        if not self:IsRightSide(dir) then
+            return
+        end
+
+        if (dir:find("/") == nil) then
+            self:Log(nil, "Launching: ", self:SideColor(), dir)
+        end
+
+        dir = dir .. "/"
+    end
+
+    local fl, folder = file_Find(dir.."*", "LUA")
+    for _, fl in ipairs(fl) do
+        if string_EndsWith(fl, ".lua") then
+            self:Include(dir, fl, tag)
+        end
+    end
+
+    if (depth == nil) then
+        depth = 0
+    else
+        depth = depth + 1
+    end
+
+    for _, fol in ipairs(folder) do
+        self:Load(dir .. fol, tag, depth)
+        self:Log(tag, fol)
+    end
+
+    if (depth == 0) then
+        Msg("\n")
     end
 end
